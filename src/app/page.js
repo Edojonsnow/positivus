a55ab3472b5f20319d623b77e1c2624b09c1dfcc
Alexander Oronsaye <osahenrumeh13@gmail.com>
2025-01-10 01:18:09 +0100

    added edge variable for cloudflare

--- src/app/page.js
+++ src/app/page.js
@@ -14,6 +14,8 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 
+export const runtime = "edge";
+
 export default function Home() {
   useEffect(() => {
     AOS.init({ once: true });
